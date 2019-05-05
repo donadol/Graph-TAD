a385320d8b839f983d3ef3edd25a375a10232031
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-05 15:52:30 -0500

    Pruebas main Jhonny agregadas

--- src/Grafos/Taller3_Grafos.java
+++ src/Grafos/Taller3_Grafos.java
@@ -104,6 +104,7 @@ public class Taller3_Grafos {
 		Map <Integer, Vertice<String>> resultadoDijkstra = grafoPrueba.dijkstra(0);
 		System.out.println (resultadoDijkstra.get(5).getContenido());
 		pruebaKruskal(grafoPrueba);
+		pruebaPrim();
 	}
 
 	static public void pruebaKruskal(Grafo<String> g) {
@@ -111,6 +112,73 @@ public class Taller3_Grafos {
 		System.out.println("Vertices en el MST con Kruskal"); 
 		for (int i = 0; i < result.size(); ++i) 
 			System.out.println(result.get(i).getOrigen().getContenido()+" -- " + result.get(i).getDestino().getContenido()+" == " + result.get(i).getCosto()); 
+	}
+	
+	private static void pruebaPrim () throws LimiteException {
+		GrafosPrueba<String> grafoPrueba = new GrafosPrueba<String>();
+		grafoPrueba.agregarVertice("0"); //0
+		grafoPrueba.agregarVertice("1"); //1
+		grafoPrueba.agregarVertice("2"); //2
+		grafoPrueba.agregarVertice("3"); //3
+		grafoPrueba.agregarVertice("4"); //4
+		grafoPrueba.agregarVertice("5"); //5
+		grafoPrueba.agregarVertice("6"); //6 
+		grafoPrueba.agregarVertice("7"); //7
+		grafoPrueba.agregarVertice("8"); //8
+		
+		
+		grafoPrueba.agregarArista(0, 1, 4);
+		grafoPrueba.agregarArista(0, 7, 8);
+		
+		grafoPrueba.agregarArista(1, 0, 4);
+		grafoPrueba.agregarArista(1, 2, 8);
+		grafoPrueba.agregarArista(1, 7, 11);
+		
+		
+		grafoPrueba.agregarArista(2, 1, 8);
+		grafoPrueba.agregarArista(2, 5, 4);
+		grafoPrueba.agregarArista(2, 8, 2);
+		grafoPrueba.agregarArista(2, 3, 7);
+		
+		
+		grafoPrueba.agregarArista(3, 2, 7);
+		grafoPrueba.agregarArista(3, 4, 9);
+		grafoPrueba.agregarArista(3, 5, 14);
+		
+		
+		grafoPrueba.agregarArista(4, 3, 9);
+		grafoPrueba.agregarArista(4, 5, 10);
+		
+		
+		grafoPrueba.agregarArista(5, 3, 14);
+		grafoPrueba.agregarArista(5, 2, 4);
+		grafoPrueba.agregarArista(5, 4, 10);
+		grafoPrueba.agregarArista(5, 6, 2);
+		
+		
+		grafoPrueba.agregarArista(6, 7, 1);
+		grafoPrueba.agregarArista(6, 5, 2);
+		grafoPrueba.agregarArista(6, 8, 6);
+		
+		
+		grafoPrueba.agregarArista(7, 0, 8);
+		grafoPrueba.agregarArista(7, 1, 11);
+		grafoPrueba.agregarArista(7, 6, 1);
+		grafoPrueba.agregarArista(7, 8, 7);
+		
+		
+		grafoPrueba.agregarArista(8, 7, 7);
+		grafoPrueba.agregarArista(8, 6, 6);
+		grafoPrueba.agregarArista(8, 2, 2);
+		
+		
+		List<Arista<String>> resultadoPrim = grafoPrueba.prim(0);
+		System.out.println ("Tamaño: "+resultadoPrim.size());
+		for (Arista <String> arista: resultadoPrim) {
+			System.out.println ("Origen: "+ arista.getOrigen().getContenido() + "-- Destino: "+arista.getDestino().getContenido());
+			
+		}
+		
 	}
 
 }
