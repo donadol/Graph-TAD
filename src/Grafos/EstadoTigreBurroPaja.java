edefe6df8d1ec4aefe50a8cb2b8d75cfd06f775b
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-05 23:43:53 -0500

    Terminado y probado punto 1
    Terminado punto Tigre, burro paja

---
+++ src/Grafos/EstadoTigreBurroPaja.java
@@ -0,0 +1,174 @@
+package Grafos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Esta clase muestra un estado en el problema del tigre, el burro y la paja
+ * Tiene dos listas:
+ * ladoA donde están las cadenas con los elementos en el ladoA
+ * ladoB donde están las cadenas con los elementos en el ladoB
+ * ladoBarca = determina el lado en el que está ubicada la barca
+ */
+public class EstadoTigreBurroPaja implements Cloneable{
+	
+	private List<String> ladoA;
+	private List<String> ladoB;
+	private char ladoBarca;
+	
+	private EstadoTigreBurroPaja estadoAnterior;
+	
+
+	public EstadoTigreBurroPaja(char lado) {
+		// TODO Auto-generated constructor stub
+		ladoA = new ArrayList<String>();
+		ladoB = new ArrayList<String>();	
+		this.ladoBarca = lado;
+	}
+
+
+	public List<String> getLadoA() {
+		return ladoA;
+	}
+
+
+	public void setLadoA(List<String> ladoA) {
+		this.ladoA = ladoA;
+	}
+
+
+	public List<String> getLadoB() {
+		return ladoB;
+	}
+
+
+	public void setLadoB(List<String> ladoB) {
+		this.ladoB = ladoB;
+	}
+
+
+	
+	
+	public char getLadoBarca() {
+		return ladoBarca;
+	}
+
+
+	public void setLadoBarca(char ladoBarca) {
+		this.ladoBarca = ladoBarca;
+	}
+
+
+	public void agregarLadoA(String elemento) {
+		ladoA.add(elemento);
+	}
+	public void agregarLadoB(String elemento) {
+		ladoB.add(elemento);
+	}
+	
+	
+	
+	
+	public EstadoTigreBurroPaja getEstadoAnterior() {
+		return estadoAnterior;
+	}
+
+
+	public void setEstadoAnterior(EstadoTigreBurroPaja estadoAnterior) {
+		this.estadoAnterior = estadoAnterior;
+	}
+
+
+	/*
+	 * Determina el tipo de estado:
+	 * -1 es inválido: 
+	 * El Tigre y el Burro están en el mismo lado y la barca no está en el lado
+	 * El Burro y la Paja están en el mismo lado y la barca no está en el lado
+	 * 
+	 * 0 si es una solución:
+	 * El Tigre, el Burro, la Paja y el Barco están en el lado B
+	 */
+	public int getTipoEstado() {
+		
+		//Lado A
+		if (ladoA.contains("Tigre") && ladoA.contains("Burro") && ladoBarca=='B')
+			return -1;
+		if (ladoA.contains("Burro") && ladoA.contains("Paja") && ladoBarca=='B')
+			return -1;
+		
+		//Lado B
+		if (ladoB.contains("Tigre") && ladoB.contains("Burro") && ladoBarca=='A')
+			return -1;
+		if (ladoB.contains("Burro") && ladoB.contains("Paja") && ladoBarca=='A')
+			return -1;
+		
+		if (ladoB.contains("Burro") && ladoB.contains("Tigre") && ladoB.contains("Paja") && ladoBarca == 'B') {
+			return 0;
+		}
+		
+		return 1;
+	}
+	
+	public String toString() {
+		String estado = "";
+		if (ladoBarca == 'A') 
+			estado += "Barca";
+		
+		for (String elementoA: ladoA) 
+			estado += " "+ elementoA;
+		
+		
+		estado += " |     | ";
+		
+		if (ladoBarca == 'B') 
+			estado += "Barca";
+		
+		for (String elementoA: ladoB) 
+			estado += " "+ elementoA;
+		
+		return estado;		
+		
+	}
+	
+	@Override
+	protected EstadoTigreBurroPaja clone() throws CloneNotSupportedException {
+		EstadoTigreBurroPaja clon = new EstadoTigreBurroPaja(this.ladoBarca);
+		clon.setLadoA(new ArrayList<>(ladoA));
+		clon.setLadoB(new ArrayList<>(ladoB));
+
+		return clon;
+	}
+	
+	public void moverElementoaA(String elemento) {
+		
+		ladoB.remove(elemento);
+		ladoA.add(elemento);
+		ladoBarca = 'A';
+		
+		
+	}
+	public void moverElementoaB(String elemento) {
+
+		ladoA.remove(elemento);
+		ladoB.add(elemento);
+		ladoBarca = 'B';
+		
+	}
+	
+	public boolean igual (EstadoTigreBurroPaja o) {
+		
+		if (ladoA.containsAll( o.getLadoA() ) && ladoB.containsAll(o.getLadoB())&& ladoBarca == o.getLadoBarca() ) {
+			return true;
+		}
+
+		
+		return false;
+	}
+	
+	
+
+	
+	
+	
+
+}
---
+++ src/Grafos/GrafoTigreBurroPaja.java
@@ -0,0 +1,167 @@
+package Grafos;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class GrafoTigreBurroPaja extends GrafoListaVecinos<EstadoTigreBurroPaja> {
+
+	// Representa los identificadores de los vertices solución, es decir, en los que
+	// el Tigre, Burro y Paja están en el lado B
+	
+	private List<Integer> identificadoresSolucion;
+
+	public GrafoTigreBurroPaja() throws CloneNotSupportedException {
+		// TODO Auto-generated constructor stub
+		identificadoresSolucion = new ArrayList<Integer>();
+		inicializarGrafo();
+		
+	}
+
+	public Vertice<EstadoTigreBurroPaja> obtenerVertice(int identificador) {
+		return this.vertices.get(identificador);
+	}
+	
+	
+
+	public List<Integer> getIdentificadoresSolucion() {
+		return identificadoresSolucion;
+	}
+
+	public void setIdentificadoresSolucion(List<Integer> identificadoresSolucion) {
+		this.identificadoresSolucion = identificadoresSolucion;
+	}
+
+	// crea el grafo con todas las posibilidades de movimiento en el problema
+	public void inicializarGrafo () throws CloneNotSupportedException{
+		Queue<Vertice<EstadoTigreBurroPaja>> colaEstados = new LinkedList<Vertice<EstadoTigreBurroPaja>>();
+
+		//se inicia con los elementos y el barco en el lado A del río
+		EstadoTigreBurroPaja estadoInicial = new EstadoTigreBurroPaja('A'); 
+		estadoInicial.agregarLadoA("Tigre");
+		estadoInicial.agregarLadoA("Burro");
+		estadoInicial.agregarLadoA("Paja");
+
+		int identificador  = this.agregarVertice(estadoInicial);
+		colaEstados.add(obtenerVertice(identificador));
+
+
+		
+		while (!colaEstados.isEmpty()) {
+			Vertice<EstadoTigreBurroPaja> estado = colaEstados.remove();
+			List<EstadoTigreBurroPaja> estadosNuevos = estados (estado.getContenido());
+			int identificadorNuevo;
+
+			for (EstadoTigreBurroPaja estadoNuevo: estadosNuevos) {
+
+				//sí el estado conduce a una solución
+				if (estadoNuevo.getTipoEstado() == 1) {
+
+					
+
+					if (estado.getContenido().getEstadoAnterior() == null) {
+						identificadorNuevo = this.agregarVertice(estadoNuevo);
+						Vertice<EstadoTigreBurroPaja> nuevoVertice = obtenerVertice(identificadorNuevo);			
+						this.agregarArista(estado.getIdentificador(), identificadorNuevo, 1);
+						colaEstados.add(obtenerVertice(identificadorNuevo));	
+					}
+
+
+					else {
+
+						if(!estado.getContenido().getEstadoAnterior().igual(estadoNuevo)) {
+
+
+							if (estado.getContenido().getLadoA().contains("Burro") 
+									&& estado.getContenido().getLadoB().contains("Tigre")
+									&& estado.getContenido().getLadoB().contains("Paja") && estado.getContenido().getLadoBarca() == 'B') {
+								if ((estadoNuevo.getLadoA().contains("Paja") && estadoNuevo.getLadoA().contains("Burro")
+										&& estadoNuevo.getEstadoAnterior().getLadoB().contains("Tigre"))||										
+										
+									(estadoNuevo.getLadoA().contains("Tigre") && estadoNuevo.getLadoA().contains("Burro")
+										&& estadoNuevo.getEstadoAnterior().getLadoB().contains("Paja"))) {
+
+								}
+								else {
+
+									identificadorNuevo = this.agregarVertice(estadoNuevo);
+									Vertice<EstadoTigreBurroPaja> nuevoVertice = obtenerVertice(identificadorNuevo);			
+									this.agregarArista(estado.getIdentificador(), identificadorNuevo, 1);
+									colaEstados.add(obtenerVertice(identificadorNuevo));	
+								}
+							}
+
+							else {			
+
+								identificadorNuevo = this.agregarVertice(estadoNuevo);
+								Vertice<EstadoTigreBurroPaja> nuevoVertice = obtenerVertice(identificadorNuevo);			
+								this.agregarArista(estado.getIdentificador(), identificadorNuevo, 1);
+								colaEstados.add(obtenerVertice(identificadorNuevo));	
+							}
+
+						}
+					}		
+				}
+				//sí el estado es una solución
+				if (estadoNuevo.getTipoEstado() == 0) {
+					identificadorNuevo = this.agregarVertice(estadoNuevo);
+					this.agregarArista(estado.getIdentificador(), identificadorNuevo, 1);
+					this.identificadoresSolucion.add(identificadorNuevo);
+
+				}
+				//sí el estado no conduce a una solución
+				if (estadoNuevo.getTipoEstado() == -1) {
+					identificadorNuevo = this.agregarVertice(estadoNuevo);
+					this.agregarArista(estado.getIdentificador(), identificadorNuevo, 1);
+				}
+
+			}
+		}
+
+	}
+
+	// Para un estado del problema genera todos los posibles estados haciendo un
+	// movimiento
+	public static List<EstadoTigreBurroPaja> estados(EstadoTigreBurroPaja estado) throws CloneNotSupportedException {
+		List<EstadoTigreBurroPaja> estadosNuevos = new ArrayList<EstadoTigreBurroPaja>();
+
+		if (estado.getLadoBarca() == 'A') {
+
+			EstadoTigreBurroPaja estadoNuevo = new EstadoTigreBurroPaja('B');
+			estadoNuevo = (EstadoTigreBurroPaja) estado.clone();
+			estadoNuevo.setEstadoAnterior(estado);
+			estadoNuevo.setLadoBarca('B');
+			estadosNuevos.add(estadoNuevo);
+
+			for (String elemento : estado.getLadoA()) {
+				estadoNuevo = new EstadoTigreBurroPaja('B');
+				estadoNuevo = (EstadoTigreBurroPaja) estado.clone();
+				estadoNuevo.setEstadoAnterior(estado);
+				estadoNuevo.moverElementoaB(elemento);
+				estadosNuevos.add(estadoNuevo);
+
+			}
+
+		} else {
+
+			EstadoTigreBurroPaja estadoNuevo = new EstadoTigreBurroPaja('A');
+			estadoNuevo = (EstadoTigreBurroPaja) estado.clone();
+			estadoNuevo.setEstadoAnterior(estado);
+			estadoNuevo.setLadoBarca('A');
+			estadosNuevos.add(estadoNuevo);
+
+			for (String elemento : estado.getLadoB()) {
+
+				estadoNuevo = new EstadoTigreBurroPaja('A');
+				estadoNuevo = (EstadoTigreBurroPaja) estado.clone();
+				estadoNuevo.setEstadoAnterior(estado);
+				estadoNuevo.moverElementoaA(elemento);
+				estadosNuevos.add(estadoNuevo);
+			}
+		}
+
+		return estadosNuevos;
+	}
+
+}
